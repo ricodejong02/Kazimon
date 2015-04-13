b930358271ab69f9cc199d471714b6ca77ba8585
ricodejong02 <ricodejong02@gmail.com>
2015-04-13 17:07:23 +0200

    Alles 0.1

---
+++ Kazimon/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Kazimon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Hplabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Updater = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 45);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 45);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(463, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 45);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Hplabel
+            // 
+            this.Hplabel.AutoSize = true;
+            this.Hplabel.Location = new System.Drawing.Point(364, 314);
+            this.Hplabel.Name = "Hplabel";
+            this.Hplabel.Size = new System.Drawing.Size(35, 13);
+            this.Hplabel.TabIndex = 1;
+            this.Hplabel.Text = "label1";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(364, 292);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 2;
+            this.NameLabel.Text = "label1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(169, -169);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button5";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(643, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(291, -169);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "button5";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Updater
+            // 
+            this.Updater.Enabled = true;
+            this.Updater.Interval = 200;
+            this.Updater.Tick += new System.EventHandler(this.Updater_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 438);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.Hplabel);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label Hplabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Updater;
+    }
+}
+
---
+++ Kazimon/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Kazimon
+{
+    public partial class Form1 : Form
+    {
+        bool YourTurn = true;
+
+        int YourHP = 0;
+        int CurrentHPYou = 0;
+        int CurrentHPOther = 0;
+        int OtherHP = 0;
+        int YourAttack = 0;
+        int OtherAttack = 0;
+        int YourDef = 0;
+        int OtherDef = 0;
+
+        int DekomanHP = 50;
+        int DekomanAttack = 17;
+        int DekomanDef = 20;
+
+        int DekominHP = 50;
+        int DekominAttack = 13;
+        int DekominDef = 22;
+
+        Random rd = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YourHP = DekomanHP;
+            YourAttack = DekomanAttack;
+            YourDef = DekomanDef;
+            CurrentHPYou = YourHP;
+            OtherHP = DekomanHP;
+            OtherAttack = DekominAttack;
+            OtherDef = DekominDef;
+            CurrentHPOther = DekominHP;
+
+          
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Updater_Tick(object sender, EventArgs e)
+        {
+
+            Hplabel.Text = CurrentHPYou + "/" + YourHP;
+            NameLabel.Text = "Dekoman";
+            label1.Text = CurrentHPOther + "/" + OtherHP;
+            label2.Text = "Dekomin";
+            button1.Text = "Tackle";
+            
+            if (CurrentHPOther <= 0)
+            {
+                MessageBox.Show("You win!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+
+            if (CurrentHPYou <= 0)
+            {
+                MessageBox.Show("You fail!");
+                Thread.Sleep(1000);
+                this.Close();
+            }
+
+            if (YourTurn == false)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                Thread.Sleep(650);
+                DoTackleOther();
+                YourTurn = true;
+            }
+
+            if (YourTurn == true)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }                  
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            YourTurn = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoTackle();
+            YourTurn = false;
+        }
+
+        public void DoTackle()
+        {
+            CurrentHPOther -= 15 + YourAttack - OtherDef;
+        }
+
+        public void DoTackleOther()
+        {
+            CurrentHPYou -= 15 + OtherAttack - YourDef;
+        }
+    }
+
+}
